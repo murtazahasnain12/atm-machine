84e77e6ea7471e55e7bad57fca86bb915d395d44
murtazahasnain12 <166008539+murtazahasnain12@users.noreply.github.com>
2024-04-11 18:42:44 +0500

    Add files via upload
    push my Atm-machine to the Github

---
+++ main.ts
@@ -0,0 +1,63 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let myBalance = 10000; // Dollar
+let myPin = 1234;
+
+let pinAnswer = await inquirer.prompt(
+    [
+
+        {
+            name:"pin",
+            message:"Enter Your Pin :",
+            type:"number"
+        }
+
+    ]
+);
+
+//  12345   ===  1234 - False
+
+if(pinAnswer.pin === myPin){
+    console.log("Correct Pin Code !!!");
+
+    let operationAns = await inquirer.prompt(
+        [
+            {
+                name:"operation",
+                message:"Please Select Option",
+                type:"list",
+                choices:["withdraw", "check balance"]
+            }
+        ]
+    );
+
+    console.log(operationAns);
+
+    if(operationAns.operation === "withdraw"){
+        let amountAns = await inquirer.prompt(
+            [
+                {
+                    name: "amount",
+                    message: "Enter Your Amount :",
+                    type: "number"
+                }
+            ]
+
+        );
+        
+        // =, -=, +=
+        myBalance -= amountAns.amount
+
+        console.log("Your Remaining Balance is : " + myBalance)
+    }
+    else if(operationAns.operation === "check balance"){
+        console.log("Your Balance is : " + myBalance);
+    }
+
+
+}
+else{
+    console.log("Incorrect Pin Number !!!")
+}
